7f7a2f012ee5b82512d6f176726541164d3b3839
hcj <913805688@qq.com>
2021-11-23 14:05:53 +0800

    首页

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Home= () => import('views/home/Home')
+const Category= () => import('views/category/Category')
+const Profile= () => import('views/profile/Profile')
+const Shopcart= () => import('views/shopcart/Shopcart')
+
+Vue.use(Router)
+
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+//在项目目录下运行 npm i vue-router@3.0 -S
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+const routes=[
+  {
+    path:'',
+    redirect: '/home'
+  },
+  {
+    path:'/home',
+    component:Home
+  },
+  {
+    path:'/shopcart',
+    component:Shopcart
+  },
+  {
+    path:'/category',
+    component:Category
+  },
+  {
+    path:'/profile',
+    component:Profile
+  }
+]
+
+export default new Router({
+  routes,
+  mode:'history'
+})
