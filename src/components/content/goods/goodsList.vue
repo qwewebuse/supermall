84d265693952acd074788514d54412c7187337e5
hcj <913805688@qq.com>
2021-12-02 16:32:06 +0800

    首页

---
+++ src/components/content/goods/goodsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="(item,index) in goods" :key="index" :goods-item="item"></goods-list-item>
+  </div>
+</template>
+
+<script>
+import goodsListItem from 'components/content/goods/goodsListItem'
+
+export default {
+    name:'goodsList',
+    components: {
+        goodsListItem,
+    },
+    props: {
+        goods:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .goods {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        padding: 2px;
+    }
+</style>
